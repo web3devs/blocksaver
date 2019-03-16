c8d6692bb76f782ff5d4c594ca105cdedebdae7f
Aaron Anderson <aaronlynanderson@gmail.com>
2019-03-16 10:39:16 -0500

    :sparkles: adds render methods to profile scene

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
-import '../App.css';
+import React, { Component } from "react";
+import "../App.css";
 
 class Profile extends Component {
   render() {
     return (
       <div className="Map">
-        Profile
+        {this.renderLifetimeCheckins()}
+        {this.renderLastCheckin()}
+        {this.renderBadges()}
+        {this.renderAccount()}
       </div>
     );
   }
+
+  renderLifetimeCheckins = () => {
+    return <div>Lifetime Checkins</div>;
+  };
+
+  renderLastCheckin = () => {
+    return <div>Last Checkin</div>;
+  };
+
+  renderBadges = () => {
+    return <div>Badges</div>;
+  };
+
+  renderAccount = () => {
+    return <div>Account</div>;
+  };
 }
 
 export default Profile;
