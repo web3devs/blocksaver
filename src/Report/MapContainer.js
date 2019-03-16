11b69a859d42cb66d2c2082fa460779d14164388
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-03-16 00:47:25 -0500

    map finally loads onto page

--- src/Report/MapContainer.js
+++ src/Report/MapContainer.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react'
 import '../App.css';
 
-import { GoogleMap, Marker } from 'react-google-maps'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-class MapContainer extends Component {
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+))
 
-  render() {
-    return (
-      <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
-      >
-        {this.props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
-      </GoogleMap>
-      )
-  }
-}
-
-
-export default MapContainer
+export default MyMapComponent
--- src/Report/Report.js
+++ src/Report/Report.js
@@ -8,7 +8,13 @@ class Report extends Component {
   render() {
     return (
       <div className="Map">
-        <MapContainer isMarkerShown />
+        <MapContainer
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
         <button>Report</button>
       </div>
     );
