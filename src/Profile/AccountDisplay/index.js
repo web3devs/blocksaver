035ababeea2234bcfa91aa15da284211b4ca519f
Aaron Anderson <aaronlynanderson@gmail.com>
2019-03-16 13:51:04 -0500

    :star: edit button on account display

--- src/Profile/AccountDisplay/index.js
+++ src/Profile/AccountDisplay/index.js
@@ -2,12 +2,22 @@ import React, { Component } from "react";
 import "./index.css";
 
 class AccountDisplay extends Component {
+  state = {
+    edit: false
+  };
   render() {
     const { title, data, ...rest } = this.props;
     return (
       <div className="Display">
         <div>{title}</div>
         <div>{data}</div>
+        <button
+          onClick={() => {
+            this.setState({ edit: !this.state.edit });
+          }}
+        >
+          {this.state.edit ? "edit" : "save"}
+        </button>
       </div>
     );
   }
