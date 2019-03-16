1f8dd82bf058f31067eac1b9b4e04b42de47f890
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-03-15 22:21:32 -0500

    add profile and report folders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './NavBar/NavBar'
+import Map from './Report/Map'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar />
+        <Map />
       </div>
     );
   }
---
+++ src/Report/Map.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="Map">
+        Map
+      </div>
+    );
+  }
+}
+
+export default Map;
